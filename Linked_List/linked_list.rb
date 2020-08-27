c5cae40eaa6ac7d248faf6b666bcd1ae42ae1840
Dominykas Mickevicius <dominykas.mickev@gmail.com>
2020-08-27 15:53:54 +0100

    Linked List in Ruby

--- Linked_List/linked_list.rb
+++ Linked_List/linked_list.rb
@@ -1,83 +1,82 @@
-require "./node.rb"
-
 class LinkedList
   attr_accessor :head, :size, :tail
-    
+  
   def initialize
-    @head = nil
-    @size = 0
+    @head = nil;
+    @size = 0;
     @tail = nil
   end
-
+  
   def append(value)
-    new_node = Node.new(value)
-    @head.nil? ? @head = new_node : @tail.next_node = new_node
+    newNode = Node.new(value)
+  
+    if head.nil?
+      @head = newNode
+    else
+      @tail.next_node = newNode
+    end
+  
     @size += 1
-    @tail = new_node
-
-    @size += 1
-    @tail = new_node
+    @tail = newNode
   end
-
+  
   def prepend(value)
-    new_node = Node.new(value)
-    new_node.next_node = @head
-    @head = new_node
+    newNode = Node.new(value)
+    newNode.next_node = @head
+    @head = newNode
+  
     @size += 1
   end
-
+  
   def at(index)
     node = @head
-    i = 0
-
-    until i == index || node.next_node == nil
+    counter = 0
+  
+    until counter == index || node.next_node == nil
       node = node.next_node
-      i += 1
+      counter += 1
     end
-
-    i == index ? node : nil
+  
+    counter == index ? node : nil
   end
-
+  
   def pop
-    new_last = self.at(size-2)
-    new_last.next_node = nil
-    @tail = new_last
+    secondLast = self.at(size - 2)
+    secondLast.next_node = nil
+    @tail = secondLast
   end
-
+  
   def contains?(value)
     node = @head
-
     until node.next_node == nil || node.value == value
       node = node.next_node
     end
-
+  
     node.value == value
   end
-
+  
   def find(value)
     node = @head
-    i = 0
-
+    counter = 0
     until node.next_node == nil || node.value == value
       node = node.next_node
-      i += 1
+      counter += 1
     end
-
-    node.value == value ? i : nil
+  
+    node.value == value ? counter : nil
   end
-
+  
   def to_s
     node = @head
     output = "#{node.value}"
-
     until node.next_node == nil
       node = node.next_node
       output << " <- #{node.value}"
     end
-
-    output << " <- nil"
+  
+    output << " <- nil "
   end
-
+  
   def insert_at(value, index)
     return nil if index < 0 || index > self.size
   
@@ -86,26 +85,26 @@ class LinkedList
     elsif index == self.size
       self.append(value)
     else
-      prev_node = self.at(index-1)
-      next_node = prev_node.next_node
-      prev_node.next_node = Node.new(value)
-      prev_node.next_node.next_node = next_node
+      prevNode = self.at(index-1)
+      nextNode = prevNode.next_node
+      prevNode.next_node = Node.new(value)
+      prevNode.next_node.next_node = nextNode
     end
   end
-
+  
   def remove_at(index)
     return nil if index < 0 || index >= self.size
-
-    if index == 0
+  
+    if index == 0 
       @head = self.at(1)
     elsif index == self.size - 1
-      new_tail = self.at(self.size-2)
-      @tail = new_tail
-      new_tail.next_node = nil
+      newTail = self.at(self.size - 2)
+      @tail = newTail
+      newTail.next_node = nil
     else
-      target_node = self.at(index)
-      prev_node = self.at(index-1)
-      prev_node.next_node = target_node.next_node
+      targetNode = self.at(index)
+      prevNode = self.at(index-1)
+      prevNode.next_node = targetNode.next_node
     end
   end
 end
---
+++ Linked_List/test.rb
@@ -0,0 +1,28 @@
+require "./linked_list.rb"
+require "./node.rb"
+
+linked_list = LinkedList.new
+
+linked_list.append(1)
+linked_list.append(4)
+linked_list.append(77)
+linked_list.append(1023)
+linked_list.append(55)
+linked_list.prepend(8)
+linked_list.prepend(111)
+
+puts linked_list
+
+
+puts linked_list.at(3)
+puts linked_list.at(15)
+
+puts linked_list.find(3)
+puts linked_list.find(15)
+
+puts linked_list.contains?(15)
+puts linked_list.contains?(111)
+
+puts linked_list.pop
+
+puts linked_list
